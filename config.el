20378bea88c132e2ebad089fe9415b4cf99ebd08
Roch D'Amour <roch.damour@gmail.com>
2019-10-30 01:00:19 -0400

    Initial commit

---
+++ config.el
@@ -0,0 +1,28 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+;; Font and font size
+(setq doom-font (font-spec :family "Pragmata Pro Mono" :size 18))
+
+;; Set initial frame size and position
+(defun my/set-initial-frame ()
+  (let* ((base-factor 0.85)
+	(a-width (* (display-pixel-width) base-factor))
+        (a-height (* (display-pixel-height) base-factor))
+        (a-left (truncate (/ (- (display-pixel-width) a-width) 2)))
+	(a-top (truncate (/ (- (display-pixel-height) a-height) 2))))
+    (set-frame-position (selected-frame) a-left a-top)
+    (set-frame-size (selected-frame) (truncate a-width)  (truncate a-height) t)))
+(setq frame-resize-pixelwise t)
+(my/set-initial-frame)
+
+(def-package! org-fancy-priorities
+  :hook (org-mode . org-fancy-priorities-mode)
+  :config
+  (setq org-fancy-priorities-list '("■" "■" "■")))
+
+(after! org
+  (map! :map org-mode-map
+        :n "M-j" #'org-metadown
+        :n "M-k" #'org-metaup)
+  (setq org-directory "~/org/"))
+
+;; Place your private configuration here
---
+++ packages.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+;;
+(package! base16-theme)
+(package! org-fancy-priorities)
