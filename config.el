efbc09fd2aece3091f87fd34dfef529c5c70e5ac
Roch D'Amour <roch.damour@gmail.com>
2019-11-06 01:03:16 -0500

    More keybings, changed font, cleanup a bit

--- config.el
+++ config.el
@@ -5,32 +5,34 @@
 (require 'base16-theme)
 
 ;; Fonts too
-(setq doom-font (font-spec :family "Pragmata Pro Mono" :size 16)
-      doom-theme 'base16-helios
-      display-line-numbers-type 'relative
-      doom-big-font (font-spec :family "Pragmata Pro Mono" :size 30))
+(setq doom-theme 'base16-helios
+      display-line-numbers-type 'relative)
 
 (if (equal (display-pixel-width) 2560)
-    (setq doom-font (font-spec :family "Pragmata Pro Mono" :size 20)
-          doom-big-font (font-spec :family "Pragmata Pro Mono" :size 36))
-  (setq doom-font (font-spec :family "Pragmata Pro Mono" :size 16)
-        doom-big-font (font-spec :family "Pragmata Pro Mono" :size 30)))
+    (setq doom-font (font-spec :family "Hack" :size 20)
+          doom-big-font (font-spec :family "Hack" :size 36))
+  (setq doom-font (font-spec :family "Hack" :size 16)
+        doom-big-font (font-spec :family "Hack" :size 30)))
 
 ;; Lets drag stuff aroung using hjk;
 (map! :ne "C-S-k" #'drag-stuff-up)
 (map! :ne "C-S-j" #'drag-stuff-down)
-(map! :ne "C-S-h" #'drag-stuff-left)
 (map! :ne "C-S-l" #'drag-stuff-right)
+(map! :ne "C-S-h" #'drag-stuff-left)
 
 (map! :ne "SPC #" #'comment-or-uncomment-region)
 (map! :ne "SPC =" #'indent-buffer)
+
+(map! :ne "SPC j g" #'dumb-jump-go)
+(map! :ne "SPC j b" #'dumb-jump-back)
+
+;; On vsplit using V, focus the new frame
+(map! :ne "SPC w V" (lambda () (interactive)(evil-window-vsplit) (other-window 1)))
 
 ;; Load additionnal configuration packages
 (load-file "~/.doom.d/personal/my-org.el")
 (load-file "~/.doom.d/defuns/utils.el")
 
-;;
-;; (js2-basic-offset 2)
-;; (js-indent-level 2)
+(map! [remap org-capture] nil)
 
 (setq fancy-splash-image "~/.doom.d/sunglass.png")
--- personal/my-org.el
+++ personal/my-org.el
@@ -7,8 +7,8 @@
   (setq org-fancy-priorities-list '("■" "■" "■")))
 
 (setq org-directory "~/org/"
-      org-journal-file (concat org-directory "journal.org")
-      org-journal-file (concat org-directory "journal.org")
+      org-todo-file (concat org-directory "todo.org")
+      org-notes-file (concat org-directory "notes.org")
       org-journal-file (concat org-directory "journal.org"))
 
 (after! org
@@ -16,9 +16,19 @@
         :n "M-j" #'org-metadown
         :n "M-k" #'org-metaup)
   (setq org-bullets-bullet-list '("◆")
-        org-todo-keyword-faces (quote (("TODO" :foreground "#dc322f" :weight bold)
-                                       ("DONE" :foreground "forest green" :weight bold :strike-through t)
-                                       ("WAITING" :foreground "turquoise" :weight bold)))
+        org-capture-templates '(("j" "Journal" entry (file+datetree org-journal-file)
+                                 "* %?\nEntered on %U\n")
+                                ("t" "Todo:" entry (file+headline org-todo-file "Todo List")
+                                 "* TODO: %?\nEntered on %U\n")
+                                ("n" "Note" entry (file org-notes-file)
+                                 "* NOTE %?\n%U" :empty-lines 1)
+                                ("N" "Note with Clipboard" entry (file org-notes-file)
+                                 "* NOTE %?\n%U\n   %c" :empty-lines 1))
+
+        org-todo-keyword-faces (quote (("TODO" :foreground "firebrick2" :weight bold)
+                                       ("DONE" :foreground "OliveDrab2" :weight bold :strike-through t)
+                                       ("CANCELLED" :foreground "chocolate1" :weight bold :strike-through t)
+                                       ("WAITING" :foreground "cyan4" :weight bold)))
         org-todo-keywords '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
                             (sequence "WAITING(w)" "|" "CANCELLED(c)"))
         org-log-done t))
